3ea7c6c546ae20cb43d679b9e6036129e5ae16db
Terry Moreland <tsmoreland@live.com>
2022-11-15 22:02:50 +0000

    implement spectre sample, much the same as the first but used spectre version of modern_win32

--- src/modern_win32_spectre_sample/main.cpp
+++ src/modern_win32_spectre_sample/main.cpp
@@ -1,20 +1,29 @@
-// modern_win32_spectre_sample.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+// Copyright (c) 2022 Terry Moreland
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
 
 #include <iostream>
+#include <format>
+#include <modern_win32/guid.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    using modern_win32::guid;
+
+    guid const id{modern_win32::new_guid()};
+
+    auto const id_string = to_string(id);
+
+    auto const formatted_id = std::format("id: {0}", id_string);
+
+    std::cout << formatted_id << "\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
