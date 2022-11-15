bd1d9b04b9c3a4e06aae70225aaaf8de795c3a8d
Terry Moreland <terry.s.moreland@gmail.com>
2022-11-15 10:33:47 +0000

    mark variables as const

--- modern_win32_sample/main.cpp
+++ modern_win32_sample/main.cpp
@@ -2,8 +2,8 @@
 #include "modern_win32/guid.h"
 
 int main() {
-    modern_win32::guid uuid{modern_win32::new_guid()};
-    std::string uuid_as_string = to_string(uuid);
+    modern_win32::guid const uuid{modern_win32::new_guid()};
+    std::string const uuid_as_string = to_string(uuid);
     std::cout << uuid_as_string << "\n";
 
     return 0;
